25d5674eeb36f2623792da388e15b7415781c3c6
Joseph Petrillo <jpetrillo119@gmail.com>
2023-03-17 19:48:47 -0400

    add simple prisma setup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient({
+  errorFormat: "pretty",
+});
 const app = express();
 const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`express server running on port ${port}`);
+  console.log(`✅ express server running on port ${port}`);
 });
