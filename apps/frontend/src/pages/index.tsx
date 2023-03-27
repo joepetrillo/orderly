da193a1118264fb7cceb941cf2944a4bc9c213d4
Joseph Petrillo <jpetrillo119@gmail.com>
2023-03-27 14:56:50 -0400

    test

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-5xl">Home Page Testing Testing</h1>
+      <h1 className="text-5xl">Home Page Testing</h1>
     </>
   );
 }
