4aa747e82390a92f37a95936aeef58b9549803ef
Joseph Petrillo <jpetrillo119@gmail.com>
2023-04-02 13:14:58 -0400

    change from a tag to nextjs Link

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
 
   useEffect(() => {
     setMobileMenuOpen(false);
-  }, [router.route]);
+  }, [router.pathname]);
 
   return (
     <header className="relative z-10">
@@ -44,30 +44,30 @@ export default function Header() {
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
-            <a
-              href="#"
+            <Link
+              href="/dashboard"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
-              Services
-            </a>
-            <a
+              Dashboard
+            </Link>
+            <Link
               href="#"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Features
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Marketplace
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Company
-            </a>
+            </Link>
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <UserButton
@@ -112,30 +112,30 @@ export default function Header() {
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
-                  <a
-                    href="#"
+                  <Link
+                    href="/dashboard"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Services
-                  </a>
-                  <a
+                    Dashboard
+                  </Link>
+                  <Link
                     href="#"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Features
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="#"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Marketplace
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="#"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Company
-                  </a>
+                  </Link>
                 </div>
                 <div className="py-6">
                   <UserButton
