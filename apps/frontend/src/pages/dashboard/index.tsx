0601566a03ba0faa4a1133d5134d7177af524a78
Joseph Petrillo <jpetrillo119@gmail.com>
2023-04-02 21:05:39 -0400

    add simple swr sample

--- apps/frontend/src/pages/dashboard/index.tsx
+++ apps/frontend/src/pages/dashboard/index.tsx
@@ -1,7 +1,22 @@
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export default function Dashboard() {
+  const { data, error, isLoading } = useSWR(
+    "https://api.github.com/repos/vercel/swr",
+    fetcher
+  );
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
+
   return (
-    <h1>
-      This will be the dashboard page! Only logged in users can view this page.
-    </h1>
+    <div className="flex flex-col items-center justify-center">
+      <p>{data.description}</p>
+      <strong>👁 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
   );
 }
