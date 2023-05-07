e4ead87756435249c3c56c8d4295403a6b6997c1
Joseph Petrillo <jpetrillo119@gmail.com>
2023-05-07 19:41:29 -0400

    update next, update clerk, use new clerk middleware

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -1,41 +1,24 @@
-import { withClerkMiddleware, getAuth } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-// Set the paths that don't require the user to be signed in
-const publicPaths = ["/", "/signin*", "/signup*", "/404"];
+export default authMiddleware({
+  publicRoutes: ["/", "/signin", "/signup", "/404"],
+  afterAuth(auth, req) {
+    // not logged in and trying to go to private page, redirect to root
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL("/signin", req.url);
+      signInUrl.searchParams.set("redirect_url", req.url);
+      return NextResponse.redirect(signInUrl);
+    }
 
-const isPublic = (path: string) => {
-  return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
-  );
-};
-
-export default withClerkMiddleware((request: NextRequest) => {
-  const { userId } = getAuth(request);
-
-  // NOT logged in and trying to access public (not protected) page, allow this
-  if (userId === null && isPublic(request.nextUrl.pathname)) {
-    return NextResponse.next();
-  }
-
-  // NOT logged in and trying to access protected page, redirect to sign in
-  if (userId === null) {
-    const signInUrl = new URL("/signin", request.url);
-    signInUrl.searchParams.set("redirect_url", request.url);
-    return NextResponse.redirect(signInUrl);
-  }
-
-  // IS logged in and trying to access public page, redirect to courses
-  if (isPublic(request.nextUrl.pathname)) {
-    const coursesURL = new URL("/courses", request.url);
-    return NextResponse.redirect(coursesURL);
-  }
-
-  // IS logged in and trying to access protected page, allow this
-  return NextResponse.next();
+    // logged in and trying to go to public page, redirect to courses
+    if (auth.userId && auth.isPublicRoute) {
+      const coursesURL = new URL("/courses", req.url);
+      return NextResponse.redirect(coursesURL);
+    }
+  },
 });
 
 export const config = {
-  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
+  matcher: ["/((?!.*\\..*|_next).*)"],
 };
